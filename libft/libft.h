02f06955207d645188a719318ca0549ef26c23d4
Mayeul DE LA SERRE <mde-la-s@e3r7p11.42.fr>
2021-12-10 22:57:56 +0100

    tests

--- libft/libft.h
+++ libft/libft.h
@@ -6,7 +6,7 @@
 /*   By: mde-la-s <mde-la-s@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/11/26 15:42:40 by mde-la-s          #+#    #+#             */
-/*   Updated: 2021/12/10 21:51:14 by mde-la-s         ###   ########.fr       */
+/*   Updated: 2021/12/10 22:40:12 by mde-la-s         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,6 +23,7 @@ typedef struct s_list
 	struct s_list	*next;
 }				t_list;
 
+int				get_next_line(char **line);
 int				ft_atoi(const char *str);
 void			ft_bzero(void *s, size_t n);
 void			*ft_calloc(size_t count, size_t size);
--- test.c
+++ test.c
@@ -7,43 +7,36 @@ int	main(int ac, char **av, char **env)
 {
 	char	*path;
 	char	**split;
-
 	char	*str;
-	char **split_str;
-	int	i = 0;
+	char	**split_str;
+	int		i;
+	int		fret;
 
 	while (1)
 	{
-	str = readline("prompt> ");
-	if (strlen(str) > 0)
-	{
-		add_history(str);
-	split_str = ft_split(str, ' ');
-	if (!strcmp("exit", split_str[0]))
-	{
-		ft_freesplit(split_str);
-		free(str);
-		break ;
-	}
-	path = getenv("PATH");
-	split = ft_split(path, ':');
-	i = 0;
-	while (split[i])
-	{
-	split[i] = strcat(split[i], "/");
-	split[i] = strcat(split[i], split_str[0]);
-		if (!access(split[i], F_OK))
-			break ;
-		i++;
-	}
-	if (fork() == 0)
-		execve(split[i], split_str, env);
-	else
-		wait(NULL);
-//	ft_freesplit(split);
-//	ft_freesplit(split_str);
-//	free(str);
-	}
+		str = readline("prompt> ");
+		if (strlen(str) > 0)
+		{
+			add_history(str);
+			split_str = ft_split(str, ' ');
+			if (!strcmp("exit", split_str[0]))
+				break ;
+			path = getenv("PATH");
+			split = ft_split(path, ':');
+			i = 0;
+			while (split[i])
+			{
+				split[i] = strcat(split[i], "/");
+				split[i] = strcat(split[i], split_str[0]);
+				if (!access(split[i], F_OK))
+					break ;
+				i++;
+			}
+			if (fork() == 0)
+				execve(split[i], split_str, env);
+			else
+				wait(NULL);
+		}
 	}
 	return 0;
 }
